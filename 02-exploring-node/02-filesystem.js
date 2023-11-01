780decafe89d5db95571cc30e5075c60b8b89f30
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-11-01 12:08:27 +0000

    Adds cli and fs

--- 02-exploring-node/02-filesystem.js
+++ 02-exploring-node/02-filesystem.js
@@ -1,13 +1,18 @@
 const fs = require("node:fs");
 
-fs.readFile('test.txt', 'utf-8', (error, data) => {
+fs.readFile("test.txt", "utf-8", (error, data) => {
 	if (error) {
 		throw error;
 	}
 
-	console.log(data)
-})
+	console.log(data);
+});
 
 // I'm loving Node!
 
-fs.writeFile()
+fs.writeFile("example.txt", "I'm loving Node!", "utf-8", (error) => {
+	if (error) {
+		console.log(error)
+	}
+	console.log("I wrote to the file!")
+});

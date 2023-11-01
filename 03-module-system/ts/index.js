ca6baf96e9e55ca31495d5da60c3b6125fb5be25
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-11-01 15:38:52 +0000

    adds set up files

---
+++ 03-module-system/ts/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const exports_js_1 = __importDefault(require("./exports.js"));
+// console.log(multiply(2, 2))
+console.log(exports_js_1.default.add(1, 2));
+console.log(exports_js_1.default.subtract(3, 2));

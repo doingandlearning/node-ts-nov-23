d167500162f94544a943d0ac698824ac4c9e2a13
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-11-03 10:41:00 +0000

    uses the enum

--- 08-events/01-events.ts
+++ 08-events/01-events.ts
@@ -9,22 +9,29 @@ import { EventEmitter } from "node:events";
 
 const fileUploaded = new EventEmitter();
 
-fileUploaded.on("woke up", (name: string, drink: string) => {
+enum FileUploadEvent {
+  WokeUp = "woke up",
+  WentToSleep = "went to sleep",
+  FileFinished = "file finished",
+  Error = "error",
+}
+
+fileUploaded.on(FileUploadEvent.WokeUp, (name: string, drink: string) => {
   // Defensive!
   console.log(`${name} woke up and would like ${drink}.`);
 });
 
-fileUploaded.on("woke up", () => {
+fileUploaded.on(FileUploadEvent.WokeUp, () => {
   // Defensive!
   console.log("Heat up the water");
 });
 
-fileUploaded.once("woke up", () => {
+fileUploaded.once(FileUploadEvent.WokeUp, () => {
   // Defensive!
   console.log("Turn on the lights!");
 });
 // prependListenerOnce
-fileUploaded.prependListener("woke up", () => {
+fileUploaded.prependListener(FileUploadEvent.WokeUp, () => {
   // In here!
   console.log("I'll be called first even though I was registered last");
 });
@@ -33,31 +40,21 @@ function doThingsBasedOnName(name: string) {
   console.log("🚨 I want to do something based on name!");
 }
 
-type FileUploadEvents = "woke up" | "went to sleep" | "file finished";
+fileUploaded.on(FileUploadEvent.WokeUp, doThingsBasedOnName);
 
-enum File {}
+fileUploaded.emit(FileUploadEvent.WokeUp, "Kevin", "coffee");
+fileUploaded.removeListener(FileUploadEvent.WokeUp, doThingsBasedOnName);
 
-fileUploaded.on("woke up", doThingsBasedOnName);
-
-fileUploaded.emit("woke up", "Kevin", "coffee");
-fileUploaded.removeListener("woke up", doThingsBasedOnName);
-
-fileUploaded.emit("woke up", "Kevin", "coffee");
-fileUploaded.removeAllListeners("woke up");
-fileUploaded.emit("woke up", "Kevin", "coffee");
+fileUploaded.emit(FileUploadEvent.WokeUp, "Kevin", "coffee");
+fileUploaded.removeAllListeners(FileUploadEvent.WokeUp);
+fileUploaded.emit(FileUploadEvent.WokeUp, "Kevin", "coffee");
 
 fileUploaded.removeAllListeners();
-fileUploaded.emit("woke up", "Kevin", "coffee");
-fileUploaded.emit("gone to sleep", "Kevin", "coffee");
+fileUploaded.emit(FileUploadEvent.WokeUp, "Kevin", "coffee");
+fileUploaded.emit(FileUploadEvent.WentToSleep, "Kevin", "coffee");
 
-fileUploaded.on("error", () => console.log("It doesn't matter!"));
-fileUploaded.emit("error");
-
-enum FileUploadEvent {
-  WokeUp = "woke up",
-  WentToSleep = "went to sleep",
-  FileFinished = "file finished",
-}
+fileUploaded.on(FileUploadEvent.Error, () => console.log("It doesn't matter!"));
+fileUploaded.emit(FileUploadEvent.Error);
 
 // Usage:
 fileUploaded.on(FileUploadEvent.WokeUp, doThingsBasedOnName);
